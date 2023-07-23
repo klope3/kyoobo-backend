5975531ff16a7d2946e2c9aa3095c46a3f19a2ad
Josh Klope <klope3@msn.com>
2023-07-23 09:58:30 -0500

    Add create account and login endpoints

--- app.ts
+++ app.ts
@@ -4,20 +4,116 @@ import { validateRequest } from "zod-express-middleware";
 import { z } from "zod";
 import { intParseableString } from "./validations";
 import { prisma } from "./client";
-import { NOT_FOUND, OK } from "./statusCodes";
+import {
+  INTERNAL_SERVER_ERROR,
+  NOT_AUTHENTICATED,
+  NOT_FOUND,
+  OK,
+  RESOURCE_CONFLICT,
+} from "./statusCodes";
 import { message } from "./utility";
+import { createUserToken, hashPassword } from "./authUtils";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send({ message: "Placeholder" });
-});
-
 //TODO:log in
+app.post(
+  "/login",
+  validateRequest({
+    body: z.object({
+      email: z.string(),
+      password: z.string(),
+    }),
+  }),
+  async (req, res) => {
+    const userWithEmail = await prisma.user.findFirst({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!userWithEmail) {
+      return res
+        .status(NOT_AUTHENTICATED)
+        .send(message("Invalid credentials."));
+    }
+
+    const isCorrectPassword = await bcrypt.compare(
+      req.body.password,
+      userWithEmail.passwordHash
+    );
+
+    if (!isCorrectPassword) {
+      return res
+        .status(NOT_AUTHENTICATED)
+        .send(message("Invalid credentials."));
+    }
+
+    const token = createUserToken(userWithEmail);
+
+    if (!token) {
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send("There was a server error.");
+    }
+
+    res.status(OK).send(token);
+  }
+);
 
 //TODO:create account
+app.post(
+  "/users",
+  validateRequest({
+    body: z.object({
+      username: z.string(),
+      email: z.string(),
+      password: z.string(),
+    }),
+  }),
+  async (req, res) => {
+    const userWithEmail = await prisma.user.findFirst({
+      where: {
+        email: req.body.email,
+      },
+    });
+    const userWithUsername = await prisma.user.findFirst({
+      where: {
+        username: req.body.username,
+      },
+    });
+    if (userWithEmail || userWithUsername) {
+      const conflictWord = userWithEmail ? "email" : "username";
+      return res
+        .status(RESOURCE_CONFLICT)
+        .send(message(`A user with that ${conflictWord} already exists.`));
+    }
+
+    const hash = hashPassword(req.body.password);
+
+    const createdUser = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        joinDate: new Date(),
+        passwordHash: hash,
+        username: req.body.username,
+      },
+    });
+
+    const token = createUserToken(createdUser);
+
+    if (!token) {
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send(message("There was a server error."));
+    }
+
+    return res.status(OK).send(token);
+  }
+);
 
 //TODO:get level by id
 app.get(
--- authUtils.ts
+++ authUtils.ts
@@ -1,7 +1,21 @@
+import { User } from "@prisma/client";
 import { hashSync } from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const salt = 11;
 
 export function hashPassword(password: string) {
   return hashSync(password, salt);
+}
+
+function createUnsecuredUserInfo(user: User) {
+  return {
+    id: user.id,
+  };
+}
+
+export function createUserToken(user: User) {
+  const info = createUnsecuredUserInfo(user);
+  const secret = process.env.JWT_SECRET;
+  return secret && jwt.sign(info, secret);
 }
