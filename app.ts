2b28c719d6816b3dc9a57fd528911c182b551c49
Josh Klope <klope3@msn.com>
2023-08-19 14:41:52 -0500

    Update cors

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-import cors from "cors";
 import express from "express";
 import { authRouter } from "./routes/auth";
 import { levelsRouter } from "./routes/levels";
@@ -6,8 +5,15 @@ import { ratingsRouter } from "./routes/ratings";
 import { usersRouter } from "./routes/users";
 
 const app = express();
+const allowedOrigins = [
+  "https://kyoobo.vercel.app",
+  "https://kyoobo-kezuqf5ix-klope3.vercel.app/",
+];
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://kyoobo.vercel.app");
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
