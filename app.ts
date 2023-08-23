0085f307c27c0726cf270573f0d92f99443da739
Josh Klope <klope3@msn.com>
2023-08-23 15:03:41 -0500

    Allow all cors if in NODE_ENV is development

--- app.ts
+++ app.ts
@@ -5,13 +5,11 @@ import { ratingsRouter } from "./routes/ratings";
 import { usersRouter } from "./routes/users";
 
 const app = express();
-const allowedOrigins = [
-  "https://kyoobo.vercel.app",
-  "https://kyoobo-kezuqf5ix-klope3.vercel.app",
-];
+const allowedOrigins = ["https://kyoobo.vercel.app"];
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  if (origin && allowedOrigins.includes(origin)) {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  if (origin && (isDevelopment || allowedOrigins.includes(origin))) {
     res.header("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
